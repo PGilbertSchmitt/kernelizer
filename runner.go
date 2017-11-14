17a0a39767517c4c986efaceec1a0a4bcb21e3e9
PGilbertSchmitt <PGilbertSchmitt@gmail.com>
2017-11-14 08:21:41 -0800

    Use kernelate

--- runner.go
+++ runner.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"image"
 	_ "image/jpeg"
+	"image/png"
 	_ "image/png"
 	"io"
 	"log"
@@ -15,7 +16,7 @@ import (
 )
 
 func main() {
-	imageName, kernelName, err := handleFlags()
+	imageName, kernelName, outputName, err := handleFlags()
 
 	if err != nil {
 		log.Fatalln(err.Error())
@@ -25,26 +26,53 @@ func main() {
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
-	_ = img.(*image.RGBA)
+	foo := img.(*image.RGBA)
 
-	_, err = getKernalData(kernelName)
+	k, err := getKernalData(kernelName)
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
+
+	newImg, err := knl.Kernelate(foo, *k)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	if outputName == "" {
+		outputName = "spooky"
+	}
+
+	f, err := os.OpenFile(outputName, os.O_RDWR, 0777)
+	if err != nil {
+		log.Println(err)
+
+		f, err = os.Create(outputName)
+		if err != nil {
+			log.Fatalln("You done fucked up now:", err)
+		}
+	}
+	defer f.Close()
+
+	png.Encode(f, newImg)
 }
 
-// Returns the reader for the image and the image for the kernel json in that order
-func handleFlags() (string, string, error) {
+// Returns the name of the input image, the image for the kernel json, and the output image in that order
+// TODO: create return object and clean up using flags
+func handleFlags() (string, string, string, error) {
 	args := os.Args[1:]
 
 	if len(args) < 2 {
-		return "", "", errors.New("You've been a bad boy. Where are the file names?")
+		return "", "", "", errors.New("You've been a bad boy. Where are the file names?")
 	}
 
-	imageName := args[0]
+	inputName := args[0]
 	kernelName := args[1]
+	outputName := ""
+	if len(args) > 2 {
+		outputName = args[2]
+	}
 
-	return imageName, kernelName, nil
+	return inputName, kernelName, outputName, nil
 }
 
 func getImageData(imageName string) (image.Image, error) {
@@ -76,5 +104,24 @@ func getKernalData(kernelName string) (*knl.Kernel, error) {
 	decoder := json.NewDecoder(buf)
 
 	decoder.Decode(&k)
+
+	// Sanity check the kernel
+	height := len(k.K)
+
+	if height < 1 {
+		return nil, errors.New("Kernel must be at least one unit wide")
+	}
+
+	if (height % 2) == 0 {
+		return nil, errors.New("Kernel must have odd width and height")
+	}
+
+	for _, row := range k.K {
+		width := len(row)
+		if width != height {
+			return nil, errors.New("Kernel must be a square")
+		}
+	}
+
 	return &k, nil
 }
